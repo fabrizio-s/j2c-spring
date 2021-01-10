f2e65f39e629952909652a1213588ce1e79af364
Fabrizio S <fabrizio.rigacci.95@gmail.com>
2021-01-10 06:55:36 +0100

    First commit.

---
+++ src/test/java/com/j2c/j2c/J2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2c.j2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
